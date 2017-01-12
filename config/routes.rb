27505cd0e470d191e7631bd0e2950e568b1020c5
Ilan Zuckerman <ilan@matomy.com>
2017-01-12 10:20:18 +0200

    added contact page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Dojit::Application.routes.draw do
   get "welcome/index"
-
+  get "welcome/contact"
   get "welcome/about"
+  
   root to: 'welcome#index'
 
   # The priority is based upon order of creation:
