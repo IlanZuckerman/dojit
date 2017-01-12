142cb1f4c17f087029a039b248c2491ba7e3c12c
Ilan Zuckerman <ilan@matomy.com>
2017-01-12 09:20:16 +0200

    First commit and README update

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dojit::Application.config.secret_token = '1987633d1ad5d79702390cbfed8ea897be5aa45bcce87dc6c7e01b249a54af3f95b09e4426f00830d8bcf311b8b6428237fed8405c07e96e4d24e4054f0064c3'
