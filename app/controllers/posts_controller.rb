7ea3e272bfa506b2cf97cc6f4d31b1e8f4577042
Ilan Zuckerman <ilan@matomy.com>
2017-01-16 11:06:01 +0200

    minor edit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,7 +20,7 @@ class PostsController < ApplicationController
 
     if @post.save
       flash[:notice] = "Post was saved."
-      # redirect_to @post
+      redirect_to @post
     else
       flash[:error] = "There was an error saving the post. Please try again."
       render :new
