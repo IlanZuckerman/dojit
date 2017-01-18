e71535d07ac63d7c2af2ae9732b12ae87d863a46
Ilan Zuckerman <ilan@matomy.com>
2017-01-18 14:27:35 +0200

    end of authentication-continued

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ gem 'rails', '3.2.22.2'
 gem 'bootstrap-sass', '3.1.1.0'
 gem 'faker'
 gem 'devise'
+gem "letter_opener", :group => :development
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
   # attr_accessible :title, :body
+  #attr_accessible :first_name, :last_name, :email, :password, :password_confirmation
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :provider, :uid, :as => [:default, :admin]
+
 end
